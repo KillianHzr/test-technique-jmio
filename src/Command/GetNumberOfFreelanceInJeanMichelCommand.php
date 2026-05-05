e385695dc603dedf00336c9b1575c1df36d16e7e
KillianHzr <killianherzer@gmail.com>
2026-05-05 22:26:19 +0200

    fix: remove die('debug') in handler and clean unused imports

--- src/Command/GetNumberOfFreelanceInJeanMichelCommand.php
+++ src/Command/GetNumberOfFreelanceInJeanMichelCommand.php
@@ -2,17 +2,12 @@
 
 namespace App\Command;
 
-use App\Dto\FreelanceJeanPaulDto;
-use App\Dto\FreelanceLinkedInDto;
-use App\Message\InsertFreelanceLinkedInMessage;
 use App\Service\FreelanceManager;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Messenger\MessageBusInterface;
-use Symfony\Component\Serializer\SerializerInterface;
 
 #[AsCommand(
     name: 'app:stats:freelances',
