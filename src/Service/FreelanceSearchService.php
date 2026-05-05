9701d880df84db40c1e8706dc8fd4db7b1bcb616
KillianHzr <killianherzer@gmail.com>
2026-05-05 22:48:59 +0200

    feat: improve search with multi-field fuzzy matching and pagination

--- src/Service/FreelanceSearchService.php
+++ src/Service/FreelanceSearchService.php
@@ -1,9 +1,11 @@
 <?php
 namespace App\Service;
 
+use Elastica\Query;
+use Elastica\Query\MatchAll;
+use Elastica\Query\MultiMatch;
 use FOS\ElasticaBundle\Finder\PaginatedFinderInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
-
 
 readonly class FreelanceSearchService
 {
@@ -14,8 +16,27 @@ readonly class FreelanceSearchService
     {
     }
 
-    public function searchFreelance(string $query): array
+    public function searchFreelance(string $query, int $page = 1, int $limit = 10): array
     {
-        return $this->freelanceFinder->find($query);
+        $elasticaQuery = $this->buildQuery($query);
+        $elasticaQuery->setFrom(($page - 1) * $limit);
+        $elasticaQuery->setSize($limit);
+
+        return $this->freelanceFinder->find($elasticaQuery);
+    }
+
+    private function buildQuery(string $query): Query
+    {
+        if ($query === '*') {
+            return new Query(new MatchAll());
+        }
+
+        $multiMatch = new MultiMatch();
+        $multiMatch->setQuery($query);
+        $multiMatch->setFields(['fullName^3', 'firstName^2', 'lastName^2', 'jobTitle^2', 'linkedInUrl']);
+        $multiMatch->setFuzziness('AUTO');
+        $multiMatch->setOperator(MultiMatch::OPERATOR_OR);
+
+        return new Query($multiMatch);
     }
 }
