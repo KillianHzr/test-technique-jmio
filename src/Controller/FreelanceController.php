b2aefa08ef74ce0bd015241ea528cbc79c5f7d7f
KillianHzr <killianherzer@gmail.com>
2026-05-05 23:06:31 +0200

    feat: add REST API endpoints for freelance search and detail

--- src/Controller/FreelanceController.php
+++ src/Controller/FreelanceController.php
@@ -3,21 +3,44 @@
 namespace App\Controller;
 
 use App\Dto\SearchFreelanceConsoDto;
+use App\Entity\Freelance;
 use App\Service\FreelanceSearchService;
+use App\Service\FreelanceSerializer;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
+use Symfony\Component\HttpKernel\Attribute\MapQueryString;
 use Symfony\Component\Routing\Attribute\Route;
 
-
-#[Route("/freelances", name: "freelances_")]
+#[Route('/api/freelances', name: 'api_freelance_')]
 class FreelanceController extends AbstractController
 {
-    #[Route(name: "search", methods: ["GET"])]
-    public function search(#[MapQueryParameter] SearchFreelanceConsoDto $dto, FreelanceSearchService $searchService): JsonResponse
+    public function __construct(
+        private readonly FreelanceSearchService $freelanceSearchService,
+        private readonly FreelanceSerializer $freelanceSerializer,
+        private readonly EntityManagerInterface $entityManager,
+    ) {}
+
+    #[Route('', name: 'search', methods: ['GET'])]
+    public function search(#[MapQueryString] SearchFreelanceConsoDto $dto = new SearchFreelanceConsoDto()): Response
     {
-        $freelanceConsos = $searchService->searchFreelance($dto->query);
-        return $this->json($freelanceConsos, Response::HTTP_OK, [], ["groups" => "freelance_conso"]);
+        $freelances = $this->freelanceSearchService->searchFreelance($dto->query, $dto->page, $dto->limit);
+        $json = $this->freelanceSerializer->serializeFreelancesConso($freelances, ['freelance_conso']);
+
+        return new Response($json, Response::HTTP_OK, ['Content-Type' => 'application/json']);
+    }
+
+    #[Route('/{id}', name: 'detail', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function detail(int $id): Response
+    {
+        $freelance = $this->entityManager->getRepository(Freelance::class)->find($id);
+
+        if (!$freelance) {
+            return $this->json(['error' => 'Freelance not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $json = $this->freelanceSerializer->serializeFreelance($freelance, ['freelance_detail']);
+
+        return new Response($json, Response::HTTP_OK, ['Content-Type' => 'application/json']);
     }
 }
--- src/Dto/SearchFreelanceConsoDto.php
+++ src/Dto/SearchFreelanceConsoDto.php
@@ -2,12 +2,15 @@
 
 namespace App\Dto;
 
+use Symfony\Component\Validator\Constraints as Assert;
 
 class SearchFreelanceConsoDto
 {
     public function __construct(
-        public string $query
-    )
-    {
-    }
+        public string $query = '*',
+        #[Assert\Range(min: 1)]
+        public int $page = 1,
+        #[Assert\Range(min: 1, max: 100)]
+        public int $limit = 10,
+    ) {}
 }
